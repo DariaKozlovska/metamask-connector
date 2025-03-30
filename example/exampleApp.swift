785c45f2f6de914846bb404a564bc95c45314551
DariaKozlovska <odriy.lovska@gmail.com>
2025-03-30 19:38:07 +0200

    Initial Commit

---
+++ example/exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  exampleApp.swift
+//  example
+//
+//  Created by Daria Kozlovska on 30/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
